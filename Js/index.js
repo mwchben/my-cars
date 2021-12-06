804d378cbb21d5b21a91f26c14aa8f788f260d77
Mwangi Benjamin <90389018+mwchben@users.noreply.github.com>
2021-12-06 14:42:55 +0300

    search term in post done😁🙄😁😁

--- Js/index.js
+++ Js/index.js
@@ -1,10 +1,13 @@
 //fetching data from the db.json
 
-const url = "http://localhost:3000/cars"; //to get it in another order http://localhost:3000/cars?_sort=saves&_order=desc
+let url = "http://localhost:3000/cars?_sort=saves&_order=desc"; //to get it in another order http://localhost:3000/cars?_sort=saves&_order=desc
 const container = document.querySelector('.blogs'); //gets the div class of index.html to inject blog
+const formSearch = document.querySelector('.search'); //gets the div class of index.html to search blog
 
-
-async function renderTheCars(){ //async f(x) 
+async function renderTheCars(term){ //async f(x) //passing a term as input to f(x) if searched.....
+    if (term){
+        url += `&q=${term}`; //the url should be a var not const for it to work
+    }
 
     const response = await fetch(url); //a promise
     const carsData = await response.json(); //a promise too
@@ -30,3 +33,10 @@ async function renderTheCars(){ //async f(x)
 
  
 window.addEventListener('DOMContentLoaded',()=>renderTheCars());
+
+formSearch.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    renderTheCars(formSearch.term.value.trim()); //.trim() removes whitespace
+})
+
+//create a search query for the posts of cars
