09c1da600b2da07eab4110b84a7be6bf3c625018
Mwangi Benjamin <90389018+mwchben@users.noreply.github.com>
2021-12-06 11:31:06 +0300

    finished with index.js

--- Js/index.js
+++ Js/index.js
@@ -1,13 +1,32 @@
 //fetching data from the db.json
 
 const url = "http://localhost:3000/cars";
+const container = document.querySelector('.blogs'); //gets the div class of index.html to inject blog
 
 
 async function renderTheCars(){ //async f(x) 
 
-    let response = await fetch(url); //a promise
-    let carsData = await response.json(); //a promise too
+    const response = await fetch(url); //a promise
+    const carsData = await response.json(); //a promise too
     console.log(carsData);
+
+    let template = '';
+
+    carsData.forEach(carData => {
+        template+=`
+            <div class="post">
+                <h2>${carData.model}</h2>
+                <p><small>${carData.saves}</small></p>
+                <p>${carData.features.slice(0,100)}</p>
+                <a href="./details.html">More on this...</a>
+
+            </div>
+        `
+    });
+
+    container.innerHTML = template;
+
+
 }
 
  
